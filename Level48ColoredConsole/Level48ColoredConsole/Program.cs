d2744b76e9848f90feed8967006f5ac0dad6134b
cdnworks <chrisdnielson@gmail.com>
2022-06-10 11:45:33 -0700

    Level 48

---
+++ Level48ColoredConsole/Level48ColoredConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿/* This program utilizes a second project containing a helper class called ColoredConsole
+ * 
+ */
+
+using ColorConsole;
+
+// Main
+
+string name = ColoredConsole.Prompt("What is your name?");
+ColoredConsole.WriteLine("Hello " + name, ConsoleColor.Green);
