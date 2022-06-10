88b52d06317b9224b7123b9e0181dbdb4056bc75
cdnworks <chrisdnielson@gmail.com>
2022-06-09 17:04:05 -0700

    Level 45

---
+++ Level45TheRobotFactory/Level45TheRobotFactory/Program.cs
@@ -0,0 +1,69 @@
+﻿/* This program is a demonstration of the ExpandoObject class
+ * 
+ * Objectives:
+ * Create a new dynamic variable, holding a reference to an ExpandoObject.
+ * 
+ * Give the dynamic object an ID property who's type is int and assign each 'robot' a new number.
+ * 
+ * Ask the user if they want to name the robot, and if they do, collect it and store it in a Name property.
+ * 
+ * Ask if they want to provide a size for the robot. If so, collect a width and height from the user
+ * and store those in Width and Height properties.
+ * 
+ * Ask if they want to choose a color for the robot. If so, store their choice in a Color property.
+ * 
+ * Display all existing properties for the robot to the console window using the following code:
+ * foreach (KeyValuePair<string, object> property in (IDictionary<string, object>)robot)
+ *      Console.WriteLine($"{property.Key}: {property.Value}");
+ * 
+ * Loop repeatedly to allow the user to design and build multiple robots.
+ */
+
+using System.Dynamic;
+
+
+// Main
+int nextID = 1;
+
+while(true)
+{
+    dynamic robot = new ExpandoObject();
+    robot.ID = nextID;
+    nextID++;
+
+    Console.WriteLine("Do you want to name the robot?");
+    if (Console.ReadLine() == "yes") 
+    {
+        Console.Write("Enter it's name: ");
+        robot.Name = Console.ReadLine();
+    }
+
+    Console.WriteLine("Do you want a specific height for the robot?");
+    if (Console.ReadLine() == "yes")
+    {
+        Console.Write("Enter it's height: ");
+        robot.Height = Convert.ToSingle(Console.ReadLine());
+    }
+
+    Console.WriteLine("Do you want a specific width for the robot?");
+    if (Console.ReadLine() == "yes")
+    {
+        Console.Write("Enter it's width: ");
+        robot.Width = Convert.ToSingle(Console.ReadLine());
+    }
+
+    Console.WriteLine("Do you want a specific color for the robot?");
+    if (Console.ReadLine() == "yes")
+    {
+        Console.Write("Enter it's color: ");
+        robot.Color = Console.ReadLine();
+    }
+
+
+    foreach (KeyValuePair<string, object> property in (IDictionary<string, object>)robot)
+        Console.WriteLine($"{property.Key}: {property.Value}");
+
+}
+
+
+
---
+++ Level45UniterOfAdds/Level45UniterOfAdds/Program.cs
@@ -0,0 +1,31 @@
+﻿/* This program contains a simple use of the dynamic keyword
+ * 
+ * public static class Adds 
+ * {
+ *      public static int Add(int a, int b) => a + b;
+ *      public static double Add(double a, double b) => a + b;
+ *      public static string Add(string a, string b) => a + b;
+ *      public static DateTime Add(DateTime a, TimeSpan b) => a + b;
+ * }
+ * 
+ * 
+ * Objectives:
+ * Make a single Add method that can replace all four of the above methods using dynamic
+ * 
+ * Add code to your main method to call the new method with two ints, two doubles, two strings and a DateTime and TimeSpan,
+ * and display the results.
+ * 
+ */
+
+
+// Main
+Console.WriteLine(Add(1,2));
+Console.WriteLine(Add(1.5,3.55));
+Console.WriteLine(Add("dong","ding"));
+Console.WriteLine(Add(DateTime.Now,TimeSpan.FromHours(1)));
+
+
+
+// Methods
+
+dynamic Add(dynamic a, dynamic b) => a + b;
